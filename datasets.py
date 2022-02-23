d442452d4e5fb6e929732ebd250757fd331c68f0
Nikolai Berezikov <nick_berezikov@me.com>
2022-02-24 01:04:41 +0300

    Add CernDatasetOneStepVelocities dataset

--- datasets.py
+++ datasets.py
@@ -7,10 +7,11 @@ import torch
 
 
 class CernDataset(Dataset):
-    def __init__(self, folder, start_moment=0, end_moment=1, max_dataset_size=None, min_file_size=9):
+    def __init__(self, folder, start_moment=0, end_moment=1, max_dataset_size=None, min_file_size=9, load_from_npy=False):
         self.root_dir = folder
         self.start = start_moment
         self.end = end_moment
+        self.load_from_npy = load_from_npy
         self.paths = []
 
         for idx in os.listdir(folder):
@@ -18,6 +19,16 @@ class CernDataset(Dataset):
                 if (os.path.exists(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/snapshot_Ed.dat') and
                     os.path.getsize(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/snapshot_Ed.dat') // 1048576 > min_file_size):
                     self.paths.append(f'{idx}/{jobresult}')
+
+        if self.load_from_npy:
+            paths_t = []
+            for path in self.paths:
+                path_t = f'{folder}/{path}/printing_VISHNew/results'
+                if (os.path.exists(f'{path_t}/y.npy') and
+                    os.path.getsize(f'{path_t}/y.npy') == 2452484):
+                    paths_t.append(path)
+            self.paths = paths_t
+
 
         if max_dataset_size is not None:
             self.paths = self.paths[:max_dataset_size]
@@ -31,16 +42,29 @@ class CernDataset(Dataset):
 
         img = np.array([], dtype=np.float32)
         y = np.array([], dtype=np.float32)
-        with open(f'{sample_path}/printing_VISHNew/results/snapshot_Ed.dat') as f:
-            for idx, line in enumerate(f):
-                if idx > 262 * self.start and idx < 262 * (self.start + 1):
-                    t = np.fromstring(" ".join(line.split()), sep = ' ', dtype=np.float32)
-                    img = np.hstack((img, t))
-                if idx > 262 * (self.end + 1) and idx < 262 * (self.end + 2):
-                    t = np.fromstring(" ".join(line.split()), sep = ' ', dtype=np.float32)
-                    y = np.hstack((y, t))
-        y_t = torch.from_numpy(y.reshape((1, 261, -1)))
-        img_t = torch.from_numpy(img.reshape((1, 261, -1)))
+        if self.load_from_npy:
+            img_t = np.reshape(np.load(f'{sample_path}/printing_VISHNew/results/img.npy'), (1, 261, 261))
+            y_t = np.reshape(np.load(f'{sample_path}/printing_VISHNew/results/y.npy'), (-1, 261, 261))
+            if self.start == 0:
+                img_t = img_t
+                y_t = y_t[self.end - 1]
+            else:
+                img_t = y_t[self.start - 1]
+                y_t = y_t[self.end - 1]
+            y_t = torch.from_numpy(y_t.reshape((1, 261, -1)))
+            img_t = torch.from_numpy(img_t.reshape((1, 261, -1)))    
+        else:
+            with open(f'{sample_path}/printing_VISHNew/results/snapshot_Ed.dat') as f:
+                for idx, line in enumerate(f):
+                    if idx > 262 * self.start and idx < 262 * (self.start + 1):
+                        t = np.fromstring(" ".join(line.split()), sep = ' ', dtype=np.float32)
+                        img = np.hstack((img, t))
+                    if idx > 262 * (self.end + 1) and idx < 262 * (self.end + 2):
+                        t = np.fromstring(" ".join(line.split()), sep = ' ', dtype=np.float32)
+                        y = np.hstack((y, t))
+            y_t = torch.from_numpy(y.reshape((1, 261, -1)))
+            img_t = torch.from_numpy(img.reshape((1, 261, -1)))
+
         y_t = y_t[:, 3:-2, 3:-2]
         img_t = img_t[:, 3:-2, 3:-2]
 
@@ -140,6 +164,94 @@ class CernDatasetFullEvo(Dataset):
             np.save(f'{sample_path}/printing_VISHNew/results/img.npy',  img)
 
             print(f'{i+1}/{num_paths} generated')
+
+
+class CernDatasetOneStepVelocities(Dataset):
+    def __init__(self, folder, start_moment=0, end_moment=1, flatten=False, predict_type=0, modified_velocities=False, max_dataset_size=None):
+        self.root_dir = folder
+        self.flatten = flatten
+        self.start_moment = start_moment
+        self.end_moment = end_moment
+        self.predict_type = predict_type
+        self.modified_velocities = modified_velocities
+        self.paths = []
+
+        for idx in os.listdir(folder):
+            for jobresult in os.listdir(f'{folder}/{idx}'):
+                if (os.path.exists(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/vx.npy') and
+                        os.path.exists(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/vy.npy') and
+                        os.path.exists(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/y.npy') and
+                        os.path.getsize(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/vx.npy') == 2452484 and
+                        os.path.getsize(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/vy.npy') == 2452484 and
+                        os.path.getsize(f'{folder}/{idx}/{jobresult}/printing_VISHNew/results/y.npy') == 2452484):
+                        self.paths.append(f'{idx}/{jobresult}')
+
+        if max_dataset_size is not None:
+            self.paths = self.paths[:max_dataset_size]
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        sample_name = self.paths[index]
+        sample_path = os.path.join(self.root_dir, sample_name)
+
+        ed_init = np.array([], dtype=np.float32)
+        ed_final = np.array([], dtype=np.float32)
+        vx_init = np.array([], dtype=np.float32)
+        vx_final = np.array([], dtype=np.float32)
+        vy_init = np.array([], dtype=np.float32)
+        vy_final = np.array([], dtype=np.float32)
+
+        ed_full = np.load(f'{sample_path}/printing_VISHNew/results/y.npy')
+        ed_full = np.reshape(ed_full, (-1, 261, 261))
+        ed_final = ed_full[self.end_moment]
+
+        vx_full = np.load(f'{sample_path}/printing_VISHNew/results/vx.npy')
+        vy_full = np.load(f'{sample_path}/printing_VISHNew/results/vy.npy')
+        vx_full = np.reshape(vx_full, (-1, 261, 261))
+        vy_full = np.reshape(vy_full, (-1, 261, 261))
+
+        if self.start_moment == 0:
+            ed_init = np.load(f'{sample_path}/printing_VISHNew/results/img.npy')
+            ed_init = np.reshape(ed_init, (261, 261))
+            vx_init = np.zeros_like(ed_init)
+            vy_init = np.zeros_like(ed_init)
+        else:
+            ed_init = ed_full[self.start_moment]
+            vx_init = vx_full[self.start_moment]
+            vy_init = vy_full[self.start_moment]
+
+        vx_final = vx_full[self.end_moment]
+        vy_final = vy_full[self.end_moment]
+
+        # V_x = V_x * Ed, V_y = V_y * Ed
+        if self.modified_velocities:
+            vx_init = vx_init * ed_init
+            vy_init = vy_init * ed_init
+            vx_final = vx_final * ed_final
+            vy_final = vy_final * ed_final
+
+        x_t = torch.from_numpy(np.array((ed_init, vx_init, vy_init)))
+        y_t = torch.from_numpy(np.array((ed_final, vx_final, vy_final)))
+
+        x_t = x_t[:, 3:-2, 3:-2]
+        y_t = y_t[:, 3:-2, 3:-2]
+
+        if self.predict_type == 0:
+            y_t = y_t[0]
+        elif self.predict_type == 1:
+            y_t = y_t[1]
+        elif self.predict_type == 2:
+            y_t = y_t[2]
+
+        if self.flatten:
+            x_t = torch.unsqueeze(torch.reshape(x_t, (-1, 256)), 0)
+            y_t = torch.unsqueeze(torch.reshape(y_t, (-1, 256)), 0)
+
+        return x_t, y_t
+
+    
 
 
 class CernDatasetMassive(Dataset):
